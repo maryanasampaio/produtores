842d938d147e69c665d22ad7a7f04ff73be54925
Maryana Sampaio <139079933+maryanasampaio@users.noreply.github.com>
2026-04-13 16:27:50 -0300

    Fix => Padronizar nomes das variáveis

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || '159753',
-  database: process.env.DB_NAME || 'produtores',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: [Produtor],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
